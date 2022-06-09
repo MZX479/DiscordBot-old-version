f6a74e070094d3d6e9418b23bc3ac9cf57459f74
MZX479 <81829597+MZX479@users.noreply.github.com>
2022-06-09 12:10:16 +0300

    Delete dist directory

--- dist/events/ready.js
+++
@@ -1,9 +0,0 @@
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = void 0;
-const event = async (bot, f, mongo)=>{
-    console.log(bot.user.tag + ' Запущен!');
-};
-var _default = event;
-exports.default = _default;
